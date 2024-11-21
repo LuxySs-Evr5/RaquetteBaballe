edad7627c12d9c0f23d16db882e1a78003289f57
ethan <ethanvr3@gmail.com>
2024-11-21 09:16:26 +0100

    add parent class forme for circle and rectangle

---
+++ src/figures/forme.cpp
@@ -0,0 +1,68 @@
+/**
+ * @file forme.cpp
+ * @author Ethan Van Ruyskensvelde (Main developer)
+ * @brief Define the class parent Forme and the class fille Circle and Rectangle
+ * @date 16/11/2024
+ *
+ */
+
+#include <allegro5/allegro_primitives.h>
+#include "forme.hpp"
+
+// ### class Forme ###
+// # Constructors #
+Forme::Forme(Point center, ALLEGRO_COLOR color)
+    : center_(center), color_(color) {}
+
+// # Getters #
+Point Forme::getCenter() const { return center_; }
+
+ALLEGRO_COLOR Forme::getColor() const { return color_; }
+
+// # Setters #
+void Forme::setCenter(const Point center) { center_ = center; }
+
+void Forme::setColor(const ALLEGRO_COLOR &color) { color_ = color; }
+
+
+// ### class Rectangle ###
+// # Constructors #
+Rectangle::Rectangle(Point center, float width, float height, ALLEGRO_COLOR color)
+    : Forme(center, color), width_(width), height_(height) {}
+
+// # Getters #
+float Rectangle::getWidth() const { return width_; }
+
+float Rectangle::getHeight() const { return height_; }
+
+// # Setters #
+void Rectangle::setWidth(const float width) { width_ = width; }
+
+void Rectangle::setHeight(const float height) { height_ = height; }
+
+// # Public Methods #
+void Rectangle::draw() {
+  const float x1 = center_.x - width_ / 2;
+  const float y1 = center_.y - height_ / 2;
+  const float x2 = center_.x + width_ / 2;
+  const float y2 = center_.y + height_ / 2;
+  al_draw_filled_rectangle(x1, y1, x2, y2, color_);
+  al_draw_rectangle(x1, y1, x2, y2, color_, 1);
+}
+
+// ### class Circle ###
+// # Constructors #
+Circle::Circle(Point center, float radius, ALLEGRO_COLOR color)
+    : Forme(center, color), radius_(radius) {}
+
+// # Getters #
+float Circle::getRadius() const { return radius_; }
+
+// # Setters #
+void Circle::setRadius(const float radius) { radius_ = radius; }
+
+// # Public Methods #
+void Circle::draw() {
+  al_draw_filled_circle(center_.x, center_.y, radius_, color_);
+  al_draw_circle(center_.x, center_.y, radius_, color_, 1);
+}
