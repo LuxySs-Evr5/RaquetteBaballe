3a5aed92a44edeb64c07e454cf35ca12e42aa172
ethan <ethanvr3@gmail.com>
2024-12-02 14:14:13 +0100

    add life and score class

---
+++ model/score/score.hpp
@@ -0,0 +1,35 @@
+/**
+ * @file score.hpp
+ * @author Ethan Van Ruyskensvelde (Main developer)
+ * @brief Define the score class
+ * @date 16/11/2024
+ *
+ */
+
+#ifndef SCORE_HPP
+#define SCORE_HPP
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Score {
+private:
+  // TODO: verify if the score is very high, so int or long int or lon long int ?
+  int score_;
+
+public:
+    Score();
+    ~Score();
+
+    void addScore(const int score);
+    void resetScore();
+    void saveScore();
+
+    int getScore() const;
+    string getScoreString() const;
+};
+
+
+#endif // SCORE_HPP
