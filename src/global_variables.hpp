4dc8445422c121398f62be78259ac1d66e7306c1
ethan <ethanvr3@gmail.com>
2024-12-31 16:04:44 +0100

    set all const to constexpr in global_variables.cpp

--- src/global_variables.hpp
+++ src/global_variables.hpp
@@ -10,77 +10,75 @@
 #define GLOBAL_VARIABLES_HPP
 
 #include "model/vec2/vec2.hpp"
-
 #include <cstdint>
-#include <string>
-
-using std::string;
 
 // ### Different paths ###
-extern const char *PATH_TO_FONT;
-extern const char *PATH_TO_MUSIC;
-extern const char *PATH_TO_HEART_IMAGE;
-extern const string PATH_TO_LEVELS;
+constexpr char PATH_TO_FONT[] =
+    "ressources/fonts/CaskaydiaCoveNerdFontMono-Regular.ttf";
+constexpr char PATH_TO_MUSIC[] = "ressources/music/arkanoid.wav";
+constexpr char PATH_TO_HEART_IMAGE[] = "ressources/images/heart.png";
+constexpr char PATH_TO_LEVELS [] = "ressources/levels/";
 
 // ### Screen ###
-extern const float SCREEN_WIDTH;
-extern const float SCREEN_HEIGHT;
-extern const double FPS;
+constexpr float SCREEN_WIDTH = 1020;
+constexpr float SCREEN_HEIGHT = 1080;
+constexpr double FPS = 125;
 
 // ### Board ###
-extern const double BOARD_WIDTH;
-extern const double BOARD_HEIGHT;
-extern const double UPS;
+constexpr double BOARD_WIDTH = 980;
+constexpr double BOARD_HEIGHT = 980;
+constexpr double UPS = 500;
 
 // ### Racket ###
-extern const double RACKET_WIDTH;
-extern const double RACKET_HEIGHT;
-extern const double RACKET_Y_POSITION;
+constexpr double RACKET_WIDTH = 100;
+constexpr double RACKET_HEIGHT = 25;
+constexpr double RACKET_Y_POSITION = 50;
 
 // ### Brick ###
-extern const double BRICK_WIDTH;
-extern const double BRICK_HEIGHT;
+constexpr double BRICK_WIDTH = 70;
+constexpr double BRICK_HEIGHT = 20;
 
 // ### Ball ###
-extern const double BALL_RADIUS;
-extern const double BALL_SPEED;
-extern const Vec2 BALL_INITIAL_DIRECTION;
+constexpr double BALL_RADIUS = 10;
+constexpr double BALL_SPEED = 500;
+const Vec2 BALL_INITIAL_DIRECTION{0, 1};
 
 // ### Bonus Pill ###
-extern const double BONUS_PILL_WIDTH;
-extern const double BONUS_PILL_HEIGHT;
-extern const double BONUS_PILL_DESCENT_SPEED;
+constexpr double BONUS_PILL_WIDTH = 10;
+constexpr double BONUS_PILL_HEIGHT = 10;
+constexpr double BONUS_PILL_DESCENT_SPEED = 200;
 
 // WideRacket
-extern const double WIDE_RACKET_WIDTH;
-extern const double WIDE_RACKET_DURATION;
+constexpr double WIDE_RACKET_WIDTH = 200;
+constexpr double WIDE_RACKET_DURATION = 10;
 
 // Lazer Bonus
-extern const double LAZER_WIDTH;
-extern const double LAZER_HEIGHT;
-extern const double LAZER_SPEED;
-extern const double LAZER_DURATION;
+constexpr double LAZER_WIDTH = 5;
+constexpr double LAZER_HEIGHT = 30;
+constexpr double LAZER_SPEED = 800;
+constexpr double LAZER_DURATION = 10;
 
 // SlowDown Bonus
-extern const double SLOW_DOWN_PROPORTIONAL_CONST;
-extern const double SLOW_DOWN_DURATION;
+constexpr double SLOW_DOWN_PROPORTIONAL_CONST = 0.2;
+constexpr double SLOW_DOWN_DURATION = 10;
 
 // ### Walls ###
-extern const double WALL_THICKNESS;
+constexpr double WALL_THICKNESS = 20;
 
 // ### Durability of bricks ###
-extern const uint8_t DURABILITY_STANDARD_BRICK;
-extern const uint8_t DURABILITY_SILVER_BRICK;
+constexpr uint8_t DURABILITY_STANDARD_BRICK = 1;
+constexpr uint8_t DURABILITY_SILVER_BRICK = 2;
 
 // ### Initial number of lives ###
-extern const unsigned INITIAL_NUM_LIVES;
+constexpr unsigned INITIAL_NUM_LIVES = 3;
 
 // ### Size of image heart ###
-extern const float HEART_IMAGE_SIZE; // because the font size is 24
+constexpr float HEART_IMAGE_SIZE = 24; // because the font size is 24
 
 // ### Size of font ###
-extern const int FONT_SIZE_24;
-extern const int FONT_SIZE_50;
-extern const int FONT_SIZE_BRICK;
+constexpr int FONT_SIZE_24 = 24;
+constexpr int FONT_SIZE_50 = 50;
+constexpr int FONT_SIZE_BRICK =
+    BRICK_HEIGHT - 3; // a bit smaller than the brick height to fit in the brick
 
 #endif // GLOBAL_VARIABLES_HPP
