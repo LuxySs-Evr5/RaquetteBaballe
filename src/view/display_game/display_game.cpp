77615fb74c0d7fd2afd41571eed020e9e1e48dd0
ethan <ethanvr3@gmail.com>
2025-01-17 23:09:39 +0100

    change name of arka to Raquette Baballe and remove old files

--- src/view/display_game/display_game.cpp
+++ src/view/display_game/display_game.cpp
@@ -148,7 +148,7 @@ void DisplayGame::draw() {
     al_clear_to_color(COLOR_BLACK); // set the color of the window to black
 
     al_draw_text(font24_, COLOR_WHITE, SCREEN_WIDTH / 2, 30,
-                 ALLEGRO_ALIGN_CENTER, "Arkanoid"); // draw the title
+                 ALLEGRO_ALIGN_CENTER, "Raquette Baballe"); // draw the title
     al_draw_text(font24_, COLOR_WHITE, SCREEN_WIDTH / 6, 50,
                  ALLEGRO_ALIGN_CENTER,
                  "Life : "); // draw the text "Life : ", the hearts will be
@@ -226,7 +226,7 @@ void DisplayGame::gameWin() {
 }
 
 void DisplayGame::gameLaunch() {
-    const char line1[] = "Welcome in the Arkanoid Game!";
+    const char line1[] = "Welcome in Raquette Baballe Game!";
     const char line2[] = "Press a key to start the game";
 
     al_draw_filled_rectangle(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT,
